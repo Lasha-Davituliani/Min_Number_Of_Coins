426f7b334a259d86e6276c731d5739b330e0f88c
Lasha-Davituliani <davituliani88@gmail.com>
2023-09-27 00:38:12 +0400

    Add project files.

---
+++ Min_Number_Of_Coins/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+public class CoinChange
+{
+    public static int MinSplit(int amount)
+    {
+        int[] coins = { 50, 20, 10, 5, 1 };
+        int count = 0;
+
+        foreach (int coin in coins)
+        {
+            int coinCount = amount / coin;
+            count += coinCount;
+            amount %= coin;
+        }
+
+        return count;
+    }
+
+    public static void Main(string[] args)
+    {
+        int amount = 17;
+        int minCoins = MinSplit(amount);
+
+        Console.WriteLine($"Minimum number of coins needed to make {amount} tetri is: {minCoins}");
+    }
+}
